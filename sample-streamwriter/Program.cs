6240a2353aec125ea281a0d2e03e04af061d3857
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-20 02:48:24 -0300

    StreamWriter sample

---
+++ sample-streamwriter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace sample_streamwriter {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = @"d:\file1.txt";
+            string targetPath = @"d:\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using(StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (var line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
