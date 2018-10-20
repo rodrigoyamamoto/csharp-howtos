e626f7744a228dfabedd7c007271dbe8260e3c73
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-20 03:03:59 -0300

    Directory sample

---
+++ sample-directory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace sample_directory {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"d:\myfolders";
+
+            try {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach(var f in folders) {
+                    Console.WriteLine(f);
+                }
+
+                Console.WriteLine();
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (var f in files) {
+                    Console.WriteLine(f);
+                }
+
+                Directory.CreateDirectory(path + @"\newFolder");
+
+                Console.ReadLine();
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
