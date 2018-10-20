a235b37d7ce40412dc018af31224a0c0928120c8
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-20 02:38:02 -0300

    Using using blocks

---
+++ sample-using-block/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace sample_using_block {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"d:\file1.txt";
+
+            try {
+                /*
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+                */
+
+                // nested usings
+                using (FileStream fs = new FileStream(path, FileMode.Open)) {
+                    using (StreamReader sr = File.OpenText(path)) {
+                        while (!sr.EndOfStream) {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e) {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
