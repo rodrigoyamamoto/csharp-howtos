9c8535ab61da1cf982b8262de6027cd55a90ea65
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-20 02:27:05 -0300

    StreamReader sample

--- sample-filestream-streamreader/Program.cs
+++ sample-filestream-streamreader/Program.cs
@@ -1,7 +1,30 @@
-﻿namespace sample_filestream_streamreader {
+﻿using System;
+using System.IO;
+
+namespace sample_filestream_streamreader {
     class Program {
         static void Main(string[] args) {
 
+            string path = @"d:\file1.txt";
+            StreamReader sr = null;
+
+            try {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                Console.ReadLine();
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            finally {
+                if (sr != null) sr.Close();
+            }
         }
     }
 }
